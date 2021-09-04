b98279fac14f67f3412b9187a64ff26b0c0cecdb
yujicosao <yujikosao@me.com>
2021-09-04 19:03:29 +0900

    feat test file

---
+++ backend/tests/Feature/AdminNewsControllerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\User;
+use App\News;
+use Faker\Generator as Faker;
+
+class AdminNewsControllerTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
