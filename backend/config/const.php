fb59cd0335d7a5814253ce0d6699e54431e192ac
kosaoseisakujo <kosaoseisakujo@gmail.com>
2021-10-25 17:19:08 +0900

    add comment

--- backend/config/const.php
+++ backend/config/const.php
@@ -2,7 +2,9 @@
 return [
     'weather' =>
         [
+            // 取得間隔調整
             'intervalHour' => -1,
+            // 地域ごとのapiUrl
             'regionUrl' => 'https://weather.tsukumijima.net/api/forecast/city/130010'
         ]
 
